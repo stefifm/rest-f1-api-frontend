7c0a782c28dfdb09ef9cb2266ff189086a848b26
Stefania Veronica Bruera <59149@sistemas.frc.utn.edu.ar>
2023-06-02 16:15:07 -0300

    Change social media and add links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,21 +7,21 @@ function Footer() {
       <span className='m-4'>-</span>
       Seguinos en:
       <a
-        href='https://www.instagram.com/'
-        className='social instagram'
+        href='https://www.linkedin.com/in/stefania-bruera/'
+        className='social linkedin'
         target='_blank'
         rel='noreferrer'>
-        <i className='fab fa-instagram'></i>
+        <i className='fab fa-linkedin'></i>
       </a>
       <a
-        href='https://twitter.com/'
+        href='https://twitter.com/stefifm'
         className='social twitter'
         target='_blank'
         rel='noreferrer'>
         <i className='fab fa-twitter-square'></i>
       </a>
       <a
-        href='https://github.com/'
+        href='https://github.com/stefifm'
         className='social github'
         target='_blank'
         rel='noreferrer'>
